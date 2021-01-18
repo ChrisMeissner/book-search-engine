76bd3af53f376195a15ddd4a313901ede5a46174
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-17 22:06:54 -0800

    add schema and resolvers

--- server/server.js
+++ server/server.js
@@ -6,16 +6,19 @@ const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const ( authMiddleware ) = require("./utils/auth");
 
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
